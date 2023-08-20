4c96747fc7fafea9dee7f09f387389cb991a436c
Daniel <danielgama1@live.com>
2023-08-20 18:03:38 -0300

    feat: Arquivos das aulas de JavaScript

---
+++ JavaScript/Aula 4 - Estrutura condicionais e repetições/script.js
@@ -0,0 +1,11 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+if(jogador1 > 0){
+    console.log('Jogador 1 marcou ponto!');
+} else if(jogador2 > 0) {
+    console.log('Jogador 1 marcou ponto!');    
+} else {
+    console.log('Ninguem marcou ponto!');
+}
