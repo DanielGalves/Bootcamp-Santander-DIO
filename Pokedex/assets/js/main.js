f5f525f517937e4b26fa7bd764dbdafaf1c7531b
Daniel <danielgama1@live.com>
2023-08-23 23:51:23 -0300

    feat: refatorado o codigo implementando o conceito de arrow function

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -6,15 +6,6 @@ const limit = 10;
 const url ='https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}';
 
 fetch(url)
-    .then(function(response) {
-        return response.json()  
-    })
-    .then(function(jsonBody){
-        console.log(jsonBody)
-    })
-    .catch(function (){
-        console.log(error)
-    })
-    .finally(function(){
-        console.log('Requisição concluida!')
-    })
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
