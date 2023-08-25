4093ca989b27c1051a76260b1f4fb9d1a8e49a9f
Daniel <danielgama1@live.com>
2023-08-25 00:32:31 -0300

    feat:

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,23 +1,16 @@
 
-
-
-const offset = 0;
-const limit = 10;
-const url =`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-
-
 function convertPokemonToLi(pokemon){
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#0${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.photo}"
                  alt="${pokemon.name}">
             </div>
         </li>
@@ -29,8 +22,7 @@ const pokemonList = document.getElementById('pokemonList')
 
 
 pokeApi.getPokemons().then((pokemons = []) => {
-
-       pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-
+    const  newHtml =pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
    })
     
